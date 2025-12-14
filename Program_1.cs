b00f303dbfc8a3306620182cbdda5a1312856873
roronewa <107262351+roronewa@users.noreply.github.com>
2025-12-14 19:21:34 +0200

    Add files via upload

---
+++ Program_1.cs
@@ -0,0 +1,47 @@
+﻿namespace pr_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+            string namber_str = Console.ReadLine();
+            float number = Convert.ToInt32(namber_str);
+            bool isnat = true;
+
+            if (number <= 1)
+            {
+                isnat = false;
+            }
+            else if (number == 2)
+            {
+                isnat = true;
+            }
+            else if (number % 2 == 0)
+            {
+                isnat = false;
+            }
+            else
+            {
+                int limit = (int)Math.Sqrt(number);
+                for (int i = 3; i * i <= number; i += 2)
+                {
+                    if (number % i == 0)
+                    {
+                        isnat = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isnat)
+            {
+                Console.WriteLine($"Число {number} є простим.");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не є простим.");
+            }
+        }
+    }
+}
---
+++ Program_2.cs
@@ -0,0 +1,49 @@
+﻿namespace pr_6_2
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введіть довжину: ");
+            string namber_str = Console.ReadLine();
+
+            Console.Write("Введіть який пароль бажаєте 1 - літери 2 - літери і цифри 3 - всі символи: ");
+            string wer = Console.ReadLine();
+            int length = Convert.ToInt32(namber_str);
+
+            string pool = "";
+
+            if (wer == "1")
+            {
+                pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+
+            else if (wer == "2")
+            {
+                pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            }
+
+            else 
+            {
+                pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+            }
+
+            Random random = new Random();
+
+            string password = "";
+
+            for (int i = 0; i < length; i++ )
+            {
+                int index = random.Next(pool.Length);
+
+                char randomChar = pool[index];
+
+                password += randomChar;
+            }
+
+            Console.WriteLine($"Згенерований пароль довжиною {length}: {password}");
+        }
+    }
+}
